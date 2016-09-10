98e1dd7f870480b435090e8e22e7fe962cb08bc6
Phil Chodrow <pchodrow@mit.edu>
2016-09-10 17:04:31 -0400

    completed summarisation

--- 2_prep_data.R
+++ 2_prep_data.R
@@ -1,4 +1,6 @@
 library(data.table)
+library(dplyr)
+library(tidyr)
 
 setwd("~/projects/spatial_complexity/applications/time_dependence")
 data_dir <- 'data/time_geo/'
@@ -12,23 +14,39 @@ data <- data.table('time' = integer(),
 
 for(file in list.files(data_dir)){
 	chunk <- fread(paste0(data_dir, file), 
-				  colClasses = col_class,
-				  col.names = col_names,
-				  verbose = F)
+				   colClasses = col_class,
+				   col.names = col_names,
+				   verbose = F)
 	agged <- chunk[,.N,by = list(tract, h_tract, time)] 
 	data <- rbind(data, agged)
 }
 
-write.csv(data, 'throughput/tract_sources.csv')
-tables()
+# OK, now time to combine with the demographics
 
+col_class <- c('integer', 'integer', 'integer', 'integer', 'integer', 'character', 'integer')
+demo <- read.csv('throughput/tract_demos.csv', colClasses = col_class) %>% tbl_df
 
+# Normalize
 
+demo <- demo %>% select(-total) %>% 
+	gather(key = race, value = n, -GEOID) %>% 
+	group_by(GEOID) %>% 
+	mutate(n = n / sum(n)) %>% 
+	spread(key = race, value = n)
 
+demo <- data.table(demo)
 
-# tab <- data %>% tbl_df() %>%
-# 	group_by(tract, h_tract, time) %>% 
-# 	summarise(n = n())
+setkeyv(data, 'h_tract')
+setkeyv(demo, 'GEOID')
 
+joined <- data[demo]
 
+tab <- joined[, j = list(
+			  Asian    = sum(Asian * N, na.rm = T),
+			  Black    = sum(Black * N, na.rm = T),
+			  Hispanic = sum(Hispanic * N, na.rm = T),
+			  Other    = sum(Other * N, na.rm = T),
+			  White    = sum(White * N, na.rm = T)
+			  ), by    = list(tract, time)]
 
+write.csv(tab, 'throughput/space_time_profiles.csv')
